ee14f873c7f9da05f65872f8ce95421d9107cc87
Sage Wibberley <sagewibberley@gmail.com>
2017-11-21 19:13:13 -0500

    Add unfinished code for editing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/program/autonomous/MauiJustMessinAround.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/program/autonomous/MauiJustMessinAround.java
@@ -1,13 +1,17 @@
 package org.firstinspires.ftc.teamcode.program.autonomous;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+@Autonomous (name = "Maui Just Messin' Around")
 public class MauiJustMessinAround extends LinearOpMode {
     TestAutonomous function = new TestAutonomous();
 
     @Override
     public void runOpMode() throws InterruptedException {
         function.setUp(hardwareMap);
+
+        waitForStart();
 
         function.move(0.25, 0, 24);
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/program/teleOp/Jan.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/program/teleOp/Jan.java
@@ -7,14 +7,11 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.program.HardwareMapConstants;
 
-import static java.lang.Math.*;
+@TeleOp (name = "Jan the Man")
+public class Jan extends OpMode{
 
-@TeleOp(name = "Drive Train")
-public class DriveTrainTest extends OpMode {
-    DcMotor fr, fl, br, bl;
-    DcMotor arm;
-    Servo leftClaw, rightClaw;
-
+    DcMotor fl,fr,bl,br,arm;
+    Servo leftClaw,rightClaw;
     final int ZERO = 0;
 
     @Override
@@ -33,46 +30,22 @@ public class DriveTrainTest extends OpMode {
         fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftClaw.setPosition(1);
+        rightClaw.setPosition(0.5);
     }
 
     @Override
     public void loop() {
-        double x = gamepad1.left_stick_x / 1.5;
-        double y = -gamepad1.left_stick_y / 1.5;
-        double turnPower = gamepad1.right_stick_x / 1.5;
 
-        if (abs(x) >= 0.05 || abs(y) >= 0.05 || abs(turnPower) >= 0.05) {
-            fr.setPower(-x + y - turnPower);
-            fl.setPower(-x - y - turnPower);
-            br.setPower(+x + y - turnPower);
-            bl.setPower(-y + x - turnPower);
-        } else {
-            fr.setPower(ZERO);
-            fl.setPower(ZERO);
-            br.setPower(ZERO);
-            bl.setPower(ZERO);
-        }
 
-        if (ZERO == 0) {
-            //good
-        }
-
-//        if (gamepad1.left_trigger >= 0.05) {
-//            arm.setPower(gamepad1.left_trigger / 2);
-//        } else if (gamepad1.right_trigger >= 0.05) {
-//            arm.setPower(-gamepad1.right_trigger / 2);
-//        } else {
-//            arm.setPower(0);
-//        }
-
-        telemetry.update();
 
         if (gamepad1.right_bumper) {
-            leftClaw.setPosition(ZERO);
-            rightClaw.setPosition(1);
-        } else {
-            leftClaw.setPosition(.6);
-            rightClaw.setPosition(.35);
+            leftClaw.setPosition(leftClaw.getPosition() + 0.01);
+            rightClaw.setPosition(rightClaw.getPosition() - 0.01);
         }
+        telemetry.addData ("Left claw is at position ", leftClaw.getPosition());
+        telemetry.addData ("Right claw is at position ", rightClaw.getPosition());
+        telemetry.update();
     }
 }
