6eeda8ad5bebd61a149c34614440aa46a7deb2a7
Charlie <calhusen@gmail.com>
2021-10-13 13:26:31 -0400

    saving the archives

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Reminder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Reminder.java
@@ -17,7 +17,7 @@ class Reminder extends OpMode {
 
     @Override
     public void init() {
-        //vibe check
+        
         //Maps the hardware to the phone. See @HardwareMapConstants
 
         //Puts the controller stick values into a variable
