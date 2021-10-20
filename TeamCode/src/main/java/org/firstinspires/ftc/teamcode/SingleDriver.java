0bd1ad2447415afe1f78e7f5516fdd8221d4d0d2
Charlie <calhusen@gmail.com>
2021-10-20 16:58:10 -0400

    created darius_code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleDriver.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-//You are on the sam_code branch
+//You are on the darius_code branch
 //Hi Sam
 
 @TeleOp(name ="Single Driver TeleOp")
