9fe33229270af979aa5641d98ae6a8924bb5047a
span3 <evanco@protonmail.com>
2021-10-22 17:23:06 -0400

    created a test TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/TestTeleOp.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.OpModes.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.Objects.DriveTrain;
+import org.firstinspires.ftc.teamcode.Objects.Grabber;
+import org.firstinspires.ftc.teamcode.Objects.Misc;
+
+
+@TeleOp(name ="Single Driver TeleOp - HME", group = "TeleOp")
+public class TestTeleOp extends OpMode {
+    //Initializing the servo objects:
+
+    DriveTrain driveTrain;
+
+    //Hello there
+    @Override
+    public void init(){
+        //Hardware mapping the servos:
+        driveTrain = DriveTrain.initDriveTrain(hardwareMap);
+
+        driveTrain.resetEncoders();
+        driveTrain.setRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    /*
+    leftFront = 2
+    leftBack = 1
+    rightFront = 3
+    rightBack = 0
+     */
+
+    @Override public void loop() {
+        //Drive Train manual control system
+        driveTrain.manualDrive(gamepad1);
+        driveTrain.checkToggleSpeed(gamepad1);
+        DriveTrain.logTelemetry(telemetry, driveTrain);
+
+    }
+
+}
