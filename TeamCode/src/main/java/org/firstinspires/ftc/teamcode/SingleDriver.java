151d730cd89032301b37a8d9ac61c285074bb30f
samuelstoke <samuelstoke82@gmail.com>
2021-12-13 16:29:28 -0500

    updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleDriver.java
@@ -15,7 +15,16 @@ public class SingleDriver extends OpMode {
     DcMotor backLeftMotor;
     DcMotor frontRightMotor;
     DcMotor backRightMotor;
-    DcMotor cradle;
+    DcMotor arm; //arm to lift objects
+    Servo leftServoGrabber; //servo grabber on the right
+    Servo rightServoGrabber; //servo grabber on the left
+    DcMotor linActuator; //linear actuator that raises objects
+
+    //prepares servo motors
+    public final static double GRABBER_START = 0.45; //starting position of servo
+    public final static double GRABBER_MAX = 0.75; //max position of servo
+    public final static double GRABBER_MIN = 0.45; //min position of servo
+    public final static double GRABBER_SPEED = 0.1; //speed at which grabber grabs that which is to be grabbed
 
     //Initiation process:
     @Override
@@ -25,10 +34,17 @@ public class SingleDriver extends OpMode {
         backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
         frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
         backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
-        arm = hardwareMap.dcMotor.get("arm");
         linActuator = hardwareMap.dcMotor.get("linActuator");
-        grabber1 = hardwareMap.dcMotor.get("grabber1");
-        grabber2 = hardwareMap.dcMotor.get("grabber2");
+        leftServoGrabber = hardwareMap.servo.get("leftServoGrabber");
+        rightServoGrabber = hardwareMap.servo.get("rightServoGrabber");
+        arm= hardwareMap.dcMotor.get("arm");
+
+        //send servos to initial position
+        leftServoGrabber.setPosition(GRABBER_START);
+        rightServoGrabber.setPosition(GRABBER_START);
+
+
+
 
 
     }
@@ -40,7 +56,8 @@ public class SingleDriver extends OpMode {
         backLeftMotor.setPower(-gamepad1.left_stick_y + -gamepad1.left_stick_x + gamepad1.right_stick_x);
         frontRightMotor.setPower(gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x);
         backRightMotor.setPower(gamepad1.left_stick_y + -gamepad1.left_stick_x + gamepad1.right_stick_x);
-        arm.setPower(gamepad1.left_trigger);
+
+
 
         // lin actuator forward if x is pressed, back if y is pressed, off else
         if (gamepad1.x) {
@@ -54,7 +71,29 @@ public class SingleDriver extends OpMode {
 
         }
 
-        //turn on servo grabber
+        //turn on servo grabber NOT DONE yet
+        if (gamepad1.b) {
+            leftServoGrabber.setPosition(GRABBER_MAX);
+            rightServoGrabber.setPosition(GRABBER_MAX);
+        }
+        else if (gamepad1.a) {
+            leftServoGrabber.setPosition(0.45);
+            rightServoGrabber.setPosition(0.45);
+        }
+
+        //raises the lift to pick up objects if triggers pushed. Still otherwise
+        if ((gamepad1.left_trigger==0 && gamepad1.right_trigger==0))
+                || (gamepad2.left_trigger!=0 && gamepad2.right_trigger!=0)) {
+            arm.setPower(0);
+        }
+        else if (gamepad1.left_trigger!=0) {
+            arm.setPower(gamepad2.left_trigger);
+        }
+        else if (gamepad1.right_trigger!=0) {
+            arm.setPower(-gamepad2.right_trigger);
+
+        }
+
 
 
     }
