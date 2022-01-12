6d9b36f705ccc2fa2b9777bf6658c1269d0206f4
Ryan <rsjh4@k12albemarle.org>
2022-01-12 16:11:18 -0500

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleDriver.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-//You are on the ryan_code branch 
+//You are on the ryan_code branch
 
 
 @TeleOp(name ="Single Driver TeleOp")
