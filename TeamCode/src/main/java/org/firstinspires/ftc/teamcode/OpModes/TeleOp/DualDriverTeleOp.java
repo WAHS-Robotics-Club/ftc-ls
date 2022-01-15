7f556350d93ece76d6f8ad2285302afe84932a6f
Charlie <calhusen@gmail.com>
2022-01-14 19:11:39 -0500

    removing old code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/DualDriverTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/DualDriverTeleOp.java
@@ -25,8 +25,6 @@ public class DualDriverTeleOp extends OpMode {
 
         driveTrain.resetEncoders();
         driveTrain.setRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        arm = hardwareMap.dcMotor.get("arm");
     }
 
     @Override public void loop(){
