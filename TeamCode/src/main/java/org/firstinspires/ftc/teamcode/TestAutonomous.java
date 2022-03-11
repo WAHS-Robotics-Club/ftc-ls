d59d773b5b9f7191118c6a7a3370a44aa0de25b3
rsjh4 <rsjh4@schls.albco>
2022-03-11 17:04:19 -0500

    soy diez anos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
@@ -16,6 +16,9 @@ import com.qualcomm.robotcore.hardware.DcMotor;
         DcMotor spool;
         DcMotor grab;
         DcMotor carousel;
+        double inches;
+        double rotations;
+        int targetPosition;
 
         @Override
         public void runOpMode() throws InterruptedException {
@@ -25,38 +28,32 @@ import com.qualcomm.robotcore.hardware.DcMotor;
             bl = hardwareMap.dcMotor.get("backLeftMotor");
             fr = hardwareMap.dcMotor.get("frontRightMotor");
             br = hardwareMap.dcMotor.get("backRightMotor");
-
             grab = hardwareMap.dcMotor.get("grab");
             spool = hardwareMap.dcMotor.get("spoolMotor");
             carousel = hardwareMap.dcMotor.get("carouselSpinner");
+
+
+
+
+            telemetry.addData("Fl power", fl.getPower());
+            telemetry.addData("Bl power", bl.getPower());
+            telemetry.addData("Fr power", fr.getPower());
+            telemetry.addData("Br power", br.getPower());
+            telemetry.update();
 
             //PLAY PHASE BUTTON PRESSED
             //Wait for the button and subsequently wait 1/4 secs to start the program:
             waitForStart();
             sleep(250);
+            rotations = inches / (4*Math.PI);
+            targetPosition =(int)(rotations*1120);
+            bl.setTargetPosition(targetPosition);
+            fl.setTargetPosition(targetPosition);
+            fr.setTargetPosition(targetPosition);
+            br.setTargetPosition(targetPosition);
+            
 
-            fl.setPower(1);
-            bl.setPower(1);
-            fr.setPower(1);
-            br.setPower(1);
-            sleep(250);
-            fl.setPower(1);
-            bl.setPower(1);
-            fr.setPower(-1);
-            br.setPower(-1);
-            sleep(500);
-            fl.setPower(1);
-            bl.setPower(1);
-            fr.setPower(1);
-            br.setPower(1);
-            sleep(250);
-            grab.setPower(0.2);
-            wait(100);
-            grab.setPower(-0.2);
-            wait(100);
-            spool.setPower(0.5);
-            wait(100);
-            grab.setPower(0.2);
+
 
 
         }
